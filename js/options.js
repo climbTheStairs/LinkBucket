a3facc2042a738b31038a1fed1ee46c753bd4fce
climbTheStairs <yxtqwf+github@gmail.com>
2023-08-29 16:02:38 -0700

    config.js: rename: options_* -> config_*

--- js/options.js
+++ js/options.js
@@ -1,15 +1,15 @@
 import { onOrIfDomContentLoaded } from "/lib/site/js/stairz.js"
 import { S, getConfig } from "/js/main.js"
 
-const options_load = async () => {
+const config_load = async () => {
 	const config = await getConfig()
 	const [$form] = document.forms
 	;[...$form].filter($in => $in.name)
 		.forEach($in => $in.checked = config[$in.name])
-	$form.addEventListener("submit", options_submit)
+	$form.addEventListener("submit", config_submit)
 }
 
-const options_submit = async function(e) {
+const config_submit = async function(e) {
 	e.preventDefault() // `return false` does not work with `async`
 	const [$form] = document.forms
 	const config = Object.fromEntries([...$form]
@@ -18,4 +18,4 @@ const options_submit = async function(e) {
 	await S.set({ config })
 }
 
-onOrIfDomContentLoaded(options_load)
+onOrIfDomContentLoaded(config_load)
