bad7093164ed2994d60066ff1e88bec616f45f8b
climbTheStairs <yxtqwf+github@gmail.com>
2023-08-29 04:21:31 -0700

    main.js: setConfig(): remove

--- js/main.js
+++ js/main.js
@@ -17,11 +17,6 @@ const QUERY_WIN_CURR = { currentWindow: true, pinned: false }
 const getConfig = async () => {
 	return Object.assign({}, DEFAULT_CONFIG, await S.get({ config: {} }))
 }
-const setConfig = async (key, val) => {
-	const config = await getConfig()
-	config[key] = val ?? DEFAULT_CONFIG[key]
-	S.set({ config })
-}
 
 const getTags = async () => {
 	const code = `window.prompt("Enter tags:")`
