aa8eb6269de83b9f900a4cfd4963fde80c4db870
climbTheStairs <yxtqwf+github@gmail.com>
2023-08-30 04:59:33 -0700

    bg.js: fmt: mv fn defs out of `cmds`
    nothing else

--- js/bg.js
+++ js/bg.js
@@ -3,45 +3,53 @@ import {
 	getTags, saveLinks, tab2link,
 } from "/js/main.js"
 
+const save_curr = async () => {
+	const tags = await getTags()
+	if (tags === null)
+		return // action aborted
+	const [tabCurr] = await T.query(QUERY_TAB_CURR)
+	await saveLinks(tab2link(tabCurr, tags))
+}
+
+const save_close_curr = async () => {
+	const tags = await getTags()
+	if (tags === null)
+		return // action aborted
+	const [tabCurr] = await T.query(QUERY_TAB_CURR)
+	await saveLinks(tab2link(tabCurr, tags))
+	await T.remove(tabCurr.id)
+}
+
+const save_all = async () => {
+	const tags = await getTags()
+	if (tags === null)
+		return // action aborted
+	const tabs = await T.query(QUERY_WIN_CURR)
+	await saveLinks(...tabs.map(tab => tab2link(tab, tags)))
+}
+
+const save_close_all = async () => {
+	const tags = await getTags()
+	if (tags === null)
+		return // action aborted
+	const tabs = await T.query(QUERY_WIN_CURR)
+	await saveLinks(...tabs.map(tab => tab2link(tab, tags)))
+	await T.remove(tabs.map(tab => tab.id))
+}
+
+const go_to_bucket = async () => {
+	const url = R.getURL("/html/bucket.html")
+	const [tabBucket] = await T.query({ currentWindow: true, url })
+	if (tabBucket)
+		await T.update(tabBucket.id, { active: true })
+	else
+		await T.create({ url: "/html/bucket.html" })
+}
+
 const cmds = {
-	save_curr: async () => {
-		const tags = await getTags()
-		if (tags === null)
-			return // action aborted
-		const [tabCurr] = await T.query(QUERY_TAB_CURR)
-		await saveLinks(tab2link(tabCurr, tags))
-	},
-	save_close_curr: async () => {
-		const tags = await getTags()
-		if (tags === null)
-			return // action aborted
-		const [tabCurr] = await T.query(QUERY_TAB_CURR)
-		await saveLinks(tab2link(tabCurr, tags))
-		await T.remove(tabCurr.id)
-	},
-	save_all: async () => {
-		const tags = await getTags()
-		if (tags === null)
-			return // action aborted
-		const tabs = await T.query(QUERY_WIN_CURR)
-		await saveLinks(...tabs.map(tab => tab2link(tab, tags)))
-	},
-	save_close_all: async () => {
-		const tags = await getTags()
-		if (tags === null)
-			return // action aborted
-		const tabs = await T.query(QUERY_WIN_CURR)
-		await saveLinks(...tabs.map(tab => tab2link(tab, tags)))
-		await T.remove(tabs.map(tab => tab.id))
-	},
-	go_to_bucket: async () => {
-		const url = R.getURL("/html/bucket.html")
-		const [tabBucket] = await T.query({ currentWindow: true, url })
-		if (tabBucket)
-			await T.update(tabBucket.id, { active: true })
-		else
-			await T.create({ url: "/html/bucket.html" })
-	},
+	save_curr, save_close_curr,
+	save_all, save_close_all,
+	go_to_bucket,
 }
 
 C.onCommand.addListener(cmd => console.debug("recv cmd: " + cmd))
