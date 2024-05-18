7a9ffad4670c8e1d72f01b33cc98f701c96d9cb8
climbTheStairs <yxtqwf+github@gmail.com>
2024-05-17 21:21:01 -0700

    config.js: fmt: slight restruct

--- js/config.js
+++ js/config.js
@@ -1,21 +1,26 @@
 import {onOrIfDomContentLoaded} from "/lib/site/js/stairz.js"
 import {S, getConfig} from "/js/main.js"
 
-const main = async () => {
+const main = () => {
+	const [$form] = document.forms
+	configLoad()
+	$form.onsubmit = configSave
+}
+
+const configLoad = async () => {
 	const config = await getConfig()
 	const [$form] = document.forms
 	;[...$form].filter($in => $in.name)
 		.forEach($in => $in.checked = config[$in.name])
-	$form.onsubmit = configSave
 }
 
-const configSave = async function(e) {
-	e.preventDefault() // `return false` does not work with `async`
+const configSave = async (e) => {
 	const [$form] = document.forms
 	const config = Object.fromEntries([...$form]
 		.filter($in => $in.name)
 		.map($in => [$in.name, $in.checked]))
 	await S.set({config})
+	e.preventDefault() // `return false` does not work with `async`
 }
 
 onOrIfDomContentLoaded(main)
