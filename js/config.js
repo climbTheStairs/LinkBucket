3d70c7a8b697de826effa6bfb33b904d983a9470
climbTheStairs <yxtqwf+github@gmail.com>
2024-05-18 22:28:41 -0700

    js/config.js: fmt: reorder

--- js/config.js
+++ js/config.js
@@ -1,26 +1,25 @@
 import {onOrIfDomContentLoaded} from "/lib/site/js/stairz.js"
 import {S, getConfig} from "/js/main.js"
 
+const [$form] = document.forms
+
 const main = () => {
-	const [$form] = document.forms
 	configLoad()
 	$form.onsubmit = configSave
 }
 
 const configLoad = async () => {
-	const [$form] = document.forms
 	const config = await getConfig()
 	;[...$form].filter($in => $in.name)
 		.forEach($in => $in.checked = config[$in.name])
 }
 
 const configSave = async (e) => {
-	const [$form] = document.forms
+	e.preventDefault() // `return false` does not work with `async`
 	const config = Object.fromEntries([...$form]
 		.filter($in => $in.name)
 		.map($in => [$in.name, $in.checked]))
 	await S.set({config})
-	e.preventDefault() // `return false` does not work with `async`
 }
 
 onOrIfDomContentLoaded(main)
