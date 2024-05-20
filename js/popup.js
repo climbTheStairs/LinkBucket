b85cb1cc71ba6c5d242d4ff6652c72c78c891205
climbTheStairs <yxtqwf+github@gmail.com>
2024-05-20 12:40:25 -0700

    [major] js/popup.js: structure

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,14 @@
 import {$, extendProto, onOrIfDomContentLoaded} from "/lib/site/js/stairz.js"
 extendProto.Element()
 
-const QUERY_TAB_CURR = {active: true, currentWindow: true}
-const QUERY_WIN_CURR = {currentWindow: true, pinned: false}
-const win = new URL(location).searchParams.get("win") === "1"
-const tabs = await browser.tabs.query(win ? QUERY_WIN_CURR : QUERY_TAB_CURR)
 const [$form] = document.forms
+const QUERY_TAB = {currentWindow: true, active: true}
+const QUERY_WIN = {currentWindow: true, pinned: false}
+const SAVE_WIN = +new URL(window.location).searchParams.get("win")
+const TABS_CURR = await browser.tabs.query(SAVE_WIN ? QUERY_WIN : QUERY_TAB)
 
 const main = () => {
-	$("#links").append(...tabs.map(tab2tr))
+	$("#links").append(...TABS_CURR.map(tab2tr))
 	$form.ts.value = new Date().toISOString()
 	browser.runtime.sendMessage("config").then(config =>
 		$form.close.checked = config.popup_close)
@@ -25,38 +25,32 @@ const tab2tr = ({title, url, favIconUrl}) => {
 }
 
 const saveTabs = async (e) => {
-	e.preventDefault() // `return false` does not work with `async`
-	const tabsToSave = [...$form.title].map((_, i) => ({
-		title: $form.title[i].value,
-		url:   $form.url[i].value,
-		favIconUrl: tabs[i].favIconUrl,
-	}))
-	const tags = $form.tags.value.split(",").map(x => x.trim())
-	const ts   = $form.ts.value.trim()
-	if (ts && Number.isNaN(+new Date(ts))) {
-		return // TODO: error handling
-	}
-	try {
-		await saveTabsAsLinks(tabsToSave, tags, ts)
-	} catch (_) {
-		return // TODO: error handling
-	}
-	if ($form.close.checked)
-		await browser.tabs.remove(tabs.map(t => t.id))
-	window.close()
-}
-
-const saveTabsAsLinks = async (tabs, tags, ts = new Date().toISOString()) => {
+	e.preventDefault()
 	let {bucket, favicons, maxId} = await browser.storage.local.get({
 		bucket: {},
 		favicons: {},
 		maxId: 0,
 	})
-	for (const {title, url, favIconUrl} of tabs) {
-		saveFavicon(favicons, url, favIconUrl)
-		bucket[++maxId] = {id: maxId, title, url, tags, ts}
+	const tags = $form.tags.value.split(",").map(x => x.trim())
+	const ts   = $form.ts.value.trim()
+	if (ts && Number.isNaN(+new Date(ts))) {
+		return // TODO: error handling
 	}
-	await browser.storage.local.set({bucket, favicons, maxId})
+	TABS_CURR.forEach(({favIconUrl}, i) => {
+		const title = ($form.title?.[i] ?? $form.title).value
+		const url   = ($form.url?.[i]   ?? $form.url  ).value
+		saveFavicon(favicons, url, favIconUrl)
+		// TODO: why not `maxId++`?
+		bucket[++maxId] = {id: maxId, title, url, tags, ts}
+	})
+	try {
+		await browser.storage.local.set({bucket, favicons, maxId})
+	} catch (_) {
+		return // TODO: error handling
+	}
+	if ($form.close.checked)
+		await browser.tabs.remove(TABS_CURR.map(t => t.id))
+	window.close()
 }
 
 const saveFavicon = (favicons, url, favicon) => {
